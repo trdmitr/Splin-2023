874ea76d510ef765c4b16f5b5c846ad431909d99
trdmitr <trdmitr@gmail.com>
2023-06-08 00:11:51 +0400

    Updates

---
+++ precache-manifest.2c9ca07ce5aad909a2882d0872a6b47e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d77ec6aeebec96858df7",
+    "url": "/Splin-2023/static/css/main.b43152d6.chunk.css"
+  },
+  {
+    "revision": "d77ec6aeebec96858df7",
+    "url": "/Splin-2023/static/js/main.a18177d2.chunk.js"
+  },
+  {
+    "revision": "5f67eba0392227e40e65",
+    "url": "/Splin-2023/static/js/runtime~main.234fca7a.js"
+  },
+  {
+    "revision": "10822aaf4c7773cce12e",
+    "url": "/Splin-2023/static/css/2.a92e3cb8.chunk.css"
+  },
+  {
+    "revision": "10822aaf4c7773cce12e",
+    "url": "/Splin-2023/static/js/2.ec5a7a66.chunk.js"
+  },
+  {
+    "revision": "326ba02ee63f8ce55f86",
+    "url": "/Splin-2023/static/js/3.82729454.chunk.js"
+  },
+  {
+    "revision": "f57c1202a8a62acee8a56622412c3e5b",
+    "url": "/Splin-2023/static/media/jakobctt_8b806cbf869f1c45daf1949a025cd519.f57c1202.woff"
+  },
+  {
+    "revision": "04ea9cf4f3491f454cb7886ae4a18ede",
+    "url": "/Splin-2023/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Splin-2023/precache-manifest.2c9ca07ce5aad909a2882d0872a6b47e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Splin-2023/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
